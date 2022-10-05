6f4ba857ad457fbd510c9dde7883a72beabc8866
Paul Fariello <paul@fariello.eu>
2022-10-05 14:55:14 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='stormbot-yocto-alert',
-      version='2.0b1',
+      version='2.0',
       description='yoctopuce plugin for stormbot',
       long_description=long_description,
       author='Paul Fariello',
       author_email='paul@fariello.eu',
       url='https://github.com/manoir/stormbot-yocto-alert',
       packages=find_packages(),
-      install_requires=['stormbot>=2.0b1', 'yoctopuce'],
+      install_requires=['stormbot>=2.0', 'yoctopuce'],
       entry_points={'stormbot.plugins': ['yocto = stormbot_yocto_alert:Yocto']},
       classifiers=['Environment :: Console',
                    'Operating System :: POSIX',
